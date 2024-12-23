58f538cb95768131e95d5ac49d947ee924f4c070
Aman Yadav <yaman6461@gmail.com>
2024-12-23 12:44:27 +0530

    fix for unwanted event propagation

--- deleteItem.js
+++ deleteItem.js
@@ -5,50 +5,63 @@ function deleteItem(index) {
   modal.show();
 
   const deleteData = document.getElementById("deletedata");
-  deleteData.addEventListener("click", function () {
-    // debugger
-    // check the expense in exist for these budget
-    const tableData = localStorage.getItem("budgetData");
-    const data = JSON.parse(tableData);
-    const dataElement = data[index];
-    let expenseData = localStorage.getItem("ExpenseData");
-    expenseData = JSON.parse(expenseData);
 
-    if (
-      expenseData.some(
-        (val) =>
-          val.ExpenseCategory === dataElement.category && 
-          val.Expensemonth === dataElement.month
-      )
-    ) {
+  const tableData = localStorage.getItem("budgetData");
+  const data = JSON.parse(tableData);
+  const dataElement = data[index];
+  let expenseData = localStorage.getItem("ExpenseData");
+  expenseData = JSON.parse(expenseData);
+
+  deleteData.addEventListener(
+    "click",
+    function () {
       // debugger
-      alert("this budegt expense already exist");
-      return;
-    }
-    data.splice(index, 1);
-    localStorage.setItem("budgetData", JSON.stringify(data));
-    modal.hide();
-    showTableData(data);
-  });
+      // check the expense in exist for these budget
+      console.log("delete balance");
+
+      if (
+        expenseData.some(
+          (val) =>
+            val.ExpenseCategory === dataElement.category &&
+            val.Expensemonth === dataElement.month
+        )
+      ) {
+        // debugger
+        alert("this budegt expense already exist");
+        return;
+      }
+      data.splice(index, 1);
+      localStorage.setItem("budgetData", JSON.stringify(data));
+      modal.hide();
+      showTableData(data);
+    },
+    { once: true }
+  );
 }
 
 function deleteItemExpense(index) {
-  const modalElement = document.getElementById("deleteModalExpense");
+  const modalElement = document.getElementById("deleteModal");
   const modal = new bootstrap.Modal(modalElement);
   modal.show();
-  const deleteData = document.getElementById("deletedataExpense");
-  deleteData.addEventListener("click", function () {
-    const tableData = localStorage.getItem("ExpenseData");
-    const data = JSON.parse(tableData);
-    data.splice(index, 1);
-    localStorage.setItem("ExpenseData", JSON.stringify(data));
-    modal.hide();
-    showExpenseData(data);
+  const deleteData = document.getElementById("deletedata");
+  deleteData.addEventListener(
+    "click",
+    function () {
+      console.log("expenese");
 
-    let storageBudgetData = localStorage.getItem("budgetData");
-    storageBudgetData = JSON.parse(storageBudgetData);
-    showTableData(storageBudgetData);
-  });
+      const tableData = localStorage.getItem("ExpenseData");
+      const data = JSON.parse(tableData);
+      data.splice(index, 1);
+      localStorage.setItem("ExpenseData", JSON.stringify(data));
+      modal.hide();
+      showExpenseData(data);
+
+      let storageBudgetData = localStorage.getItem("budgetData");
+      storageBudgetData = JSON.parse(storageBudgetData);
+      showTableData(storageBudgetData);
+    },
+    { once: true }
+  );
 }
 
 export { deleteItem, deleteItemExpense };
