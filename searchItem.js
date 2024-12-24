4c9954a2922ebbd22d417118fe135af38d05249f
souravsaini1681 <souravsaini1681@gmail.com>
2024-12-24 12:13:18 +0530

    second change of tuesday

--- searchItem.js
+++ searchItem.js
@@ -6,14 +6,18 @@ function searchData() {
     .value.toLowerCase();
   let storageData = localStorage.getItem("ExpenseData");
   storageData = JSON.parse(storageData) ?? [];
-  const filterData = storageData.filter(
-    (item) =>
+  
+  const filterData = storageData.filter((item) => {
+    const expenseAmountStr = item.ExpenseAmount.toString();
+    return (
       item.ExpenseCategory.toLowerCase().includes(searchInput) ||
       item.ExpenseTitle.toLowerCase().includes(searchInput) ||
       item.Expensemonth.toLowerCase().includes(searchInput) ||
-      item.ExpenseAmount.includes(searchInput)
-  );
+      expenseAmountStr.includes(searchInput)
+    );
+  });
+
   showExpenseData(filterData);
-}   
+}
 
 export default searchData;
